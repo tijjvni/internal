1d5353c109837571c94f81a50190565654807150
tjyusuf <tjyusuf@Yahoo.com>
2022-01-06 09:38:18 +0000

    msg

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -1,8 +1,11 @@
-{{--
-    @extends('layouts.app')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Client') }}
+        </h2>
+    </x-slot>
 
-    @section('slot')
-        clients
-        <!-- @livewire('client.index', ['clients' => $clients]) -->
-    @endsection
---}}
+    <div class="py-12">
+        @livewire('client.index', ['clients' => $clients])
+    </div>
+</x-app-layout>
