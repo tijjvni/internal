73466508748a3ce0d88623520b247ea45879b194
tijjvni <tijjvni@gmail.com>
2022-01-08 12:56:59 +0000

    msge

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="flex">
+        <div class="flex w-full">
             <div class="flex-grow">
                 <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                     {{ __('Clients') }}
