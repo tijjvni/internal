7539b7c128668ffee7b9a01856a3c42bae3ab1bd
tijjvni <tijjvni@gmail.com>
2022-01-09 14:09:10 +0000

    msg

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -3,14 +3,15 @@
         <div class="flex w-full">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ __('Clients') }}
-                <button type="button" class="float-rightinline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                Create
-                </button>  
+                <a type="{{ route('clients.create') }}" class="float-right inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Create
+                </a>  
             </h2>
         </div>
     </x-slot>
 
     <div class="p-5">
-            {{count($clients)}}
-    </div>
+            @livewire('clients.create',[
+            'clients' => $clients
+        ]);    </div>
 </x-app-layout>
