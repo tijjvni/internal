b0206b10f78cc43a97ec5da812aa0977eeb2055d
tijjvni <tijjvni@gmail.com>
2022-01-09 15:19:08 +0000

    chngd

--- resources/views/livewire/clients/index.blade.php
+++ resources/views/livewire/clients/index.blade.php
@@ -44,7 +44,7 @@
                                         </span>
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                        <a href="/client/{{$client->idate}}" class="text-indigo-600 hover:text-indigo-900">view</a>
+                                        <a href="/clients/{{ $client->id }}" class="text-indigo-600 hover:text-indigo-900">view</a>
                                     </td>
                                 </tr>
                             @endforeach
