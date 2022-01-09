6828a3308ea583e5434ae8dd72dc0624e40c11e2
tijjvni <tijjvni@gmail.com>
2022-01-09 14:14:46 +0000

    msg

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -3,7 +3,7 @@
         <div class="flex w-full">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ __('Clients') }}
-                <a type="{{ route('clients.create') }}" class="float-right inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <a href="{{ route('clients.create') }}" class="float-right inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Create
                 </a>  
             </h2>
@@ -11,7 +11,8 @@
     </x-slot>
 
     <div class="p-5">
-            @livewire('clients.create',[
+            @livewire('clients.index',[
             'clients' => $clients
-        ]);    </div>
+        ]);    
+    </div>
 </x-app-layout>
