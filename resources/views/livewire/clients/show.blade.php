ca054a5c2c9e300769280c0db2105a2f76211706
tijjvni <tijjvni@gmail.com>
2022-01-12 23:01:51 +0000

    msge

--- resources/views/livewire/clients/show.blade.php
+++ resources/views/livewire/clients/show.blade.php
@@ -1,34 +1,6 @@
 <div>
 
     <!-- Code block starts -->
-    <div class="my-6 lg:my-12 container px-6 mx-auto flex flex-col md:flex-row items-start md:items-center justify-between pb-4 border-b border-gray-300">
-        <div>
-            <h4 class="text-2xl font-bold leading-tight text-gray-800 dark:text-gray-100"></h4>
-            <ul aria-label="current Status" class="flex flex-col md:flex-row items-start md:items-center text-gray-600 dark:text-gray-400 text-sm mt-3">
-                <li class="flex items-center mr-4">
-                    <div class="mr-1">
-                        <i class="fa fa-sign-in"></i>
-                    </div>
-                    <span>Active</span>
-                </li>
-                <li class="flex items-center mt-4 md:mt-0">
-                    <div class="mr-1">
-                        <img class="dark:hidden" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/simple_with_sub_text_and_border-svg3.svg" alt="date">
-                        <img class="dark:block hidden" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/simple_with_sub_text_and_border-svg3dark.svg" alt="date">
-                    </div>
-                    <span>{{ $client->created_at }}</span>
-                </li>
-            </ul>
-        </div>
-        <div class="mt-6 md:mt-0">
-            <button class="mr-3 bg-gray-200 dark:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 transition duration-150 ease-in-out rounded hover:bg-gray-300 text-indigo-700 dark:hover:bg-gray-600 dark:text-indigo-600 px-5 py-2 text-sm">Back</button>
-            <button class="transition focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-8 py-2 text-sm">Edit Profile</button>
-        </div>
-    </div>
-    <!-- Code block ends -->
-
-
-    <!-- Code block starts -->
     <div class="my-6 container px-6 mx-auto flex flex-row md:flex-row items-start md:items-center justify-between pb-4 border-b border-gray-300">
         <div class="flex-grow">
             <h4 class="text-2xl font-bold leading-tight text-gray-800 dark:text-gray-100">{{$client->name}}</h4>
