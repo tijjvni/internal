1ad845f36d588fddaf481fc1feb862efe6ebace9
tijjanidev <tijjanidev@gmail.com>
2022-01-17 11:48:28 +0000

    msg

--- resources/views/livewire/staff/index.blade.php
+++ resources/views/livewire/staff/index.blade.php
@@ -1,3 +1,3 @@
 <div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+    {{-- If your happine ss depends on money, you will never be happy with yourself. --}}
 </div>
